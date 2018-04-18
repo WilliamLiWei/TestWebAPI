6045c08e45dfd39fb586049082fb55e067bc9366
William Li <liweimyfild_0@163.com>
2018-04-18 16:57:10 +0800

    change project file to current folder

---
+++ Domains/TestTableDomain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DBTest.Models;
+
+namespace Domains
+{
+    public class TestTableDomain
+    {
+        //负责整理TestTable的业务整理
+        
+        public TestTableDomain()
+        {
+        }
+
+        public TestTableEntity AddNewTestTable(string addr, string name, int sex)
+        {
+            TestTableEntity testTableEntity = new TestTableEntity()
+            {
+                Id = Guid.NewGuid(),
+                Address = addr,
+                Name = name,
+                Sex = sex
+            };
+            return testTableEntity;
+        }
+    }
+}
