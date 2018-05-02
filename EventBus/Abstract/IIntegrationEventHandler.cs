f56f7641885882b11082a2b163ea7f0bd8f98c82
William Li <liweimyfild_0@163.com>
2018-05-02 17:36:25 +0800

    添加事件类

---
+++ EventBus/Abstract/IIntegrationEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using EventBus.Event;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Abstract
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+         where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+        
+    }
+}
